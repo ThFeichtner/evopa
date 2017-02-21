767e2355b000063b531ee13caf34b1296cf90343
ThFeichtner <thorsten.feichtner@tuep-texte.de>
2017-02-21 12:52:54 +0100

    added antenna class (empty)

---
+++ antenna.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+class antenna:
+    
+    ''' The object definition of an single antenna, which represents an
+    individual wthin a geenration of antennas within the evolutionary
+    algorithm'''
